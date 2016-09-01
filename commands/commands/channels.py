9f5be72c0f4647de68a1494239a5473c2d3ad60a
Tehom <brannigd@hotmail.com>
2016-09-01 03:29:38 -0400

    Getting channels working, hurray. Decided to just have Arx channels replace existing Evennia channels, since ours have functionality the players have grown accustomed to. Other difference is using Msg objects instead of a flat log file, which makes it easier to have dates and so on, and not needing to worry about periodically pruning the log file or whatever, though Griatch had a point that a log file does make more sense than storing things in the database. Oh well, we can switch if we ever extend the log files to handle those issues.

---
+++ commands/commands/channels.py
@@ -0,0 +1,123 @@
+"""
+A slightly different implementation of channels specific to arx.
+We use Msg objects for our chat history rather than log files,
+and have some different switches/commands, such as who, last, etc.
+
+Minor changes overall, but with functionality players have become
+accustomed to.
+"""
+
+from evennia.commands import command
+from evennia.comms.models import ChannelDB, Msg
+from evennia.utils.utils import datetime_format
+
+class ArxChannelCommand(command.Command):
+    """
+    {channelkey} channel
+
+    {channeldesc}
+
+    Usage:
+       {lower_channelkey}  <message>
+       {lower_channelkey} last
+       {lower_channelkey}/history
+       {lower_channelkey} who
+
+    This is a channel. If you have subscribed to it, you can send to
+    it by entering its name or alias, followed by the text you want to
+    send.
+    """
+    # this flag is what identifies this cmd as a channel cmd
+    # and branches off to the system send-to-channel command
+    # (which is customizable by admin)
+    is_channel = True
+    key = "general"
+    help_category = "Channel Names"
+    obj = None
+    display_history = False
+    num_messages = 20
+
+    def parse(self):
+        """
+        Simple parser
+        """
+        # cmdhandler sends channame:msg here.
+        channelname, msg = self.args.split(":", 1)
+        if msg.startswith("/history"):
+            self.display_history = True
+            arg = msg[8:]
+            try:
+                self.num_messages = int(arg) if arg else 20
+            except ValueError:
+                pass
+        else:
+            self.display_history = False
+            self.num_messages = 20
+        self.args = (channelname.strip(), msg.strip())
+
+    def func(self):
+        """
+        Create a new message and send it to channel, using
+        the already formatted input.
+        """
+        channelkey, msg = self.args
+        caller = self.caller
+        player = caller.db.player_ob
+        if not msg:
+            self.msg("Say what?")
+            return
+        channel = ChannelDB.objects.get_channel(channelkey)
+
+        if not channel:
+            self.msg("Channel '%s' not found." % channelkey)
+            return
+        if msg == "on":
+            if player: caller = player
+            caller.execute_cmd("addcom %s" % channelkey)
+            return
+        if not channel.has_connection(caller):
+            string = "You are not connected to channel '%s'."
+            self.msg(string % channelkey)
+            return
+        if not channel.access(caller, 'send'):
+            string = "You are not permitted to send to channel '%s'."
+            self.msg(string % channelkey)
+            return
+        if "%r" in msg or "{/" in msg or "|/" in msg:
+            caller.msg("Channel messages may not contain newline characters.")
+            return
+        if msg == "who" or msg == "?" or msg == "all" or msg == "list":
+            if player: caller = player
+            caller.execute_cmd("@cwho %s" % channelkey)
+            return
+        if msg == 'last' or msg.startswith("last "):
+            msglist = msg.split()
+            # check if it wasn't just a message starting with 'last'
+            # eg: 'last week we blah blah'
+            if len(msglist) == 1 or (len(msglist) == 2 and msglist[1].isdigit()):
+                self.num_messages = 20
+                if len(msglist) == 2: self.num_messages = int(msglist[1])
+                self.display_history = True
+        if self.display_history:
+            chan_messages = list(Msg.objects.get_messages_by_channel(channel.id))
+            if len(chan_messages) > 200:
+                del_list = chan_messages[:-200]
+                chan_messages = chan_messages[-200:]
+                for mssg in del_list:
+                    channel.delete_chan_message(mssg)
+            caller.msg("Messages in %s:\n" % channel.key)
+            if len(chan_messages) > self.num_messages:
+                last_messages = chan_messages[-self.num_messages:]
+            else:
+                last_messages = chan_messages
+            for msg in last_messages:
+                caller.msg("{w%s{n %s" % (datetime_format(msg.date_sent), msg.message))
+            return
+        if msg == "off":
+            if player: caller = player
+            caller.execute_cmd("delcom %s" % channelkey)
+            return
+        channel.msg(msg, senders=self.caller, persistent=True, online=True)
+        if Msg.objects.get_messages_by_channel(channel.id).count() > 200:
+            earliest = Msg.objects.get_messages_by_channel(channel.id).earliest('db_date_sent')
+            channel.delete_chan_message(earliest)
--- typeclasses/channels.py
+++ typeclasses/channels.py
@@ -13,6 +13,8 @@ to be modified.
 """
 
 from evennia import DefaultChannel
+from evennia.comms.models import Msg
+from evennia.utils.utils import make_iter
 
 class Channel(DefaultChannel):
     """
@@ -57,4 +59,135 @@ class Channel(DefaultChannel):
         post_send_message(msg) - called just after message was sent to channel
 
     """
-    pass
+    def delete_chan_message(self, message):
+        """
+        When given a message object, if the message has other
+        receivers, just remove the channels inside the message so
+        that the other receivers don't lose the message. Otherwise,
+        delete it completely.
+        """
+        if self not in message.channels:
+            return
+        if message.receivers:
+            # remove the channel from the message, but leave msg
+            # intact for other receivers
+            del message.channels
+            return
+        message.delete()
+        
+    def distribute_message(self, msg, online=False):
+        """
+        Method for grabbing all listeners that a message should be sent to on
+        this channel, and sending them a message.
+        """
+        # get all players connected to this channel and send to them
+        for entity in self.subscriptions.all():
+            try:
+                entity.msg(msg.message, from_obj=msg.senders,
+                           options={"from_channel":self.id})
+            except AttributeError as e:
+                logger.log_trace("%s\nCannot send msg to %s" % (e, entity))
+
+    def channel_prefix(self, msg=None, emit=False):
+        """
+        How the channel should prefix itself for users. Return a string.
+        """
+        # use color if defined
+        if self.db.colorstr:
+            return '%s[%s]{n ' % (self.db.colorstr, self.key)
+        # else default is whether it's private or not
+        if self.locks.get('listen').strip() != "listen:all()":
+            return '{y[%s]{n ' % self.key
+        return '{w[%s]{n ' % self.key
+
+    def pose_transform(self, msg, sender_string):
+        """
+        Detects if the sender is posing, and modifies the message accordingly.
+        """
+        pose = False
+        message = msg.message
+        message_start = message.lstrip()
+        if message_start.startswith((':', ';')):
+            pose = True
+            message = message[1:]
+            if not message.startswith((':', "'", ',')):
+                if not message.startswith(' '):
+                    message = ' ' + message
+        sender_string = "{c%s{n" % sender_string
+        if pose:
+            return '%s%s' % (sender_string, message)
+        else:
+            return '%s: %s' % (sender_string, message)
+   
+    def msg(self, msgobj, header=None, senders=None, sender_strings=None,
+            persistent=False, online=False, emit=False, external=False):
+        """
+        Send the given message to all players connected to channel. Note that
+        no permission-checking is done here; it is assumed to have been
+        done before calling this method. The optional keywords are not used if
+        persistent is False.
+
+        msgobj - a Msg/TempMsg instance or a message string. If one of the
+                 former, the remaining keywords will be ignored. If a string,
+                 this will either be sent as-is (if persistent=False) or it
+                 will be used together with header and senders keywords to
+                 create a Msg instance on the fly.
+        senders - an object, player or a list of objects or players.
+                 Optional if persistent=False.
+        sender_strings - Name strings of senders. Used for external
+                connections where the sender is not a player or object. When
+                this is defined, external will be assumed.
+        external - Treat this message agnostic of its sender.
+        persistent (default False) - ignored if msgobj is a Msg or TempMsg.
+                If True, a Msg will be created, using header and senders
+                keywords. If False, other keywords will be ignored.
+        online (bool) - If this is set true, only messages people who are
+                online. Otherwise, messages all players connected. This can
+                make things faster, but may not trigger listeners on players
+                that are offline.
+        emit (bool) - Signals to the message formatter that this message is
+                not to be directly associated with a name.
+        """
+        if senders:
+            senders = make_iter(senders)
+        else:
+            senders = []
+        if isinstance(msgobj, basestring):
+            # given msgobj is a string
+            msg = msgobj
+            # saves message if the channel allows logging and message should be persistent
+            if persistent and self.db.keep_log:
+                msgobj = Msg()
+            else:
+                # Use TempMsg, so this message is not stored.
+                msgobj = TempMsg()
+            msgobj.header = header
+            msgobj.message = msg
+            msgobj.channels = [self]  # add this channel
+
+        if not msgobj.senders:
+            msgobj.senders = senders
+        msgobj = self.pre_send_message(msgobj)
+        if not msgobj:
+            return False
+        msgobj = self.message_transform(msgobj, emit=emit,
+                                        sender_strings=sender_strings,
+                                        external=external)
+        self.distribute_message(msgobj, online=online)
+        self.post_send_message(msgobj)
+        # need save at end to capture all attributes of saved Msg()
+        if persistent and self.db.keep_log:
+            msgobj.save()
+        return True
+
+    def tempmsg(self, message, header=None, senders=None):
+        """
+        A wrapper for sending non-persistent messages. Note that this will
+        still be a persistent message if the channel's logging is turned on.
+        By default, channel logging is False, so a temp message being captured
+        should only happen by intent.
+        """
+        self.msg(message, senders=senders, header=header, persistent=False)
+
+
+
