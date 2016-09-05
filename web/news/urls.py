f6784ae9f2a5b5ecbb713708e5537acbe2600420
Tehom <surly.mime@gmail.com>
2016-09-05 02:47:36 -0400

    Fixed deprecated stuff in news app

--- web/news/urls.py
+++ web/news/urls.py
@@ -4,10 +4,11 @@ It is imported from the root handler, game.web.urls.py.
 """
 
 from django.conf.urls import *
+from . import views
 
-urlpatterns = patterns('web.news.views',
-     (r'^show/(?P<entry_id>\d+)/$', 'show_news'),
-     (r'^archive/$', 'news_archive'),
-     (r'^search/$', 'search_form'),
-     (r'^search/results/$', 'search_results'),
-)
+urlpatterns = [
+     (r'^show/(?P<entry_id>\d+)/$', views.show_news),
+     (r'^archive/$', views.news_archive),
+     (r'^search/$', views.search_form),
+     (r'^search/results/$', views.search_results),
+]
