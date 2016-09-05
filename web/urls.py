55c65b031fc0cbbaaae4e2e3180cc41aef8acf6c
Tehom <surly.mime@gmail.com>
2016-09-05 02:23:51 -0400

    Fix in deprecated patterns in root url conf

--- web/urls.py
+++ web/urls.py
@@ -4,26 +4,20 @@ views. Search the Django documentation for "URL dispatcher" for more
 help.
 
 """
-from django.conf.urls import url, include, patterns
+import django
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 
 # default evennia patterns
 from evennia.web.urls import urlpatterns
-
-### eventual custom patterns
-##custom_patterns = [
-##    # url(r'/desired/url/', view, name='example'),
-##]
-##
-### this is required by Django.
-##urlpatterns = custom_patterns + urlpatterns
+from web.website.views import to_be_implemented
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     # User Authentication
-    url(r'^accounts/login',  'django.contrib.auth.views.login'),
-    url(r'^accounts/logout', 'django.contrib.auth.views.logout'),
+    url(r'^accounts/login',  django.contrib.auth.views.login),
+    url(r'^accounts/logout', django.contrib.auth.views.logout),
 
     # Front page
     url(r'^', include('web.website.urls')),
@@ -31,14 +25,11 @@ urlpatterns = patterns('',
     url(r'^news/', include('web.news.urls')),
 
     # Page place-holder for things that aren't implemented yet.
-    url(r'^tbi/', 'web.website.views.to_be_implemented'),
+    url(r'^tbi/', to_be_implemented),
 
     # Admin interface
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-
-##    # favicon
-##    url(r'^favicon\.ico$',  RedirectView.as_view(url='/media/images/favicon.ico', permanent=False)),
 
     # ajax stuff
     url(r'^webclient/',include('evennia.web.webclient.urls',
@@ -53,16 +44,16 @@ urlpatterns = patterns('',
                           namespace='dominion', app_name='dominion')),
     url(r'^comms/', include('world.msgs.urls',
                           namespace='msgs', app_name='msgs')),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+    url(r'^static/(?P<path>.*)$', django.views.static.serve,
         {'document_root': settings.STATIC_ROOT}),
     url(r'^support/', include('web.helpdesk.urls')),
                        
-)
+]
 
 # This sets up the server if the user want to run the Django
 # test server (this should normally not be needed).
 if settings.SERVE_MEDIA:
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    urlpatterns += [
+        (r'^media/(?P<path>.*)$', django.views.static.serve, {'document_root': settings.MEDIA_ROOT}),
         
-    )
+    ]
