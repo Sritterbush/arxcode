994b8d1df67e3d64c839243ef87a9c33f744460e
Dave Brannigan <brannigd@hotmail.com>
2017-02-21 16:18:50 +0000

    adding shortrels categories for regions

--- commands/commands/roster.py
+++ commands/commands/roster.py
@@ -1228,7 +1228,8 @@ class CmdRelationship(MuxPlayerCommand):
     help_category = "Social"
     locks = "cmd:all()"
     typelist = ['parent', 'sibling', 'friend', 'enemy', 'frenemy', 'family', 'client', 'patron', 'protege',
-                'acquaintance', 'secret', 'rival', 'ally', 'spouse']
+                'acquaintance', 'secret', 'rival', 'ally', 'spouse', 'The Crown', 'Crownlands', 'Oathlands',
+                'Lyceum', 'Mourning Isles', 'Northlands']
 
     def func(self):
         caller = self.caller
