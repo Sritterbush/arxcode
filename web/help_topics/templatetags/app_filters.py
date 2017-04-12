cd510d2569992e496b26801f68a98d3d17bf6ecd
Tehom <surly.mime@gmail.com>
2017-04-12 16:33:32 -0400

    Add IC date to journal pages, add more complex search options.
    Support added for excluding names from query by prepending "-" to names, and for searching multiple names simultaneously.
    Added new app filter for extracting the IC date from a Msg header.

--- web/help_topics/templatetags/app_filters.py
+++ web/help_topics/templatetags/app_filters.py
@@ -1,9 +1,9 @@
 from django import template
-from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 from evennia.utils.ansi import parse_ansi
 
 register = template.Library()
+
 
 @register.filter
 def mush_to_html(value):
@@ -17,11 +17,27 @@ def mush_to_html(value):
     value = value.replace('\n', '<br>')
     value = value.replace('%b', ' ')
     value = value.replace('%t', '&nbsp&nbsp&nbsp&nbsp')
-    #value = value.replace('{w', '<strong>')
-    #value = value.replace('{n', '</strong>')
     value = parse_ansi(value, strip_ansi=True)
     return mark_safe(value)
+
 
 @register.filter
 def doc_str(value):
     return value.__doc__
+
+
+@register.filter
+def date_from_header(header):
+    """
+    When given a Msg object's header, extract and return IC date
+    Args:
+        header: str
+
+    Returns:
+        str: IC date of the header
+    """
+    hlist = header.split(";")
+    keyvalpairs = [pair.split(":") for pair in hlist]
+    keydict = {pair[0].strip(): pair[1].strip() for pair in keyvalpairs if len(pair) == 2}
+    date = keydict.get('date', None)
+    return date
