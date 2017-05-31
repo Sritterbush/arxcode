5a569645fb7f699395509c53ee421cc20178ef0a
Dave Brannigan <brannigd@hotmail.com>
2017-05-31 08:10:42 +0000

    fixed bug by checking if id exists before running appearance script

--- typeclasses/consumable/appearance_script.py
+++ typeclasses/consumable/appearance_script.py
@@ -12,10 +12,21 @@ class AppearanceScript(Script):
         self.start_delay = True
 
     def at_repeat(self):
+        # check if we still exist by checking if we have an id
+        if not self.id:
+            try:
+                self.stop()
+            except Exception:
+                import traceback
+                traceback.print_exc()
+            return
+        # we still exist, so check our scent and remaining time
         if self.db.scent:
             self.db.scent_time_remaining -= self.interval
+            # out of time. remove our scent
             if self.db.scent_time_remaining <= 0:
                 self.db.scent = ""
+        # no longer modifying anything, call stop
         if not self.has_mods:
             self.stop()
 
@@ -25,7 +36,15 @@ class AppearanceScript(Script):
 
     @property
     def has_mods(self):
-        return self.db.scent
+        """
+        Checks if we have anything modifying our appearance. Currently this
+        is just scent. More things to be added later.
+        
+            Returns:
+                bool: Strong. Like bull. Would defy being judged by Walrii
+        """
+        return bool(self.db.scent)
 
     def is_valid(self):
-        return self.db.scent
+        # check having a valid ID (we're not deleted) and our db.scent
+        return bool(self.id and self.has_mods)
