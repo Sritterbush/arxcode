0a2e49807e5e58b7e00a66b46d5b65b43769cee3
Pax <seattlesparks@mac.com>
2017-12-26 02:08:08 -0800

    Silly map experiment.

--- world/dominion/views.py
+++ world/dominion/views.py
@@ -1,12 +1,13 @@
 from django.views.generic import ListView, DetailView
-from .models import RPEvent, AssignedTask, Crisis
+from .models import RPEvent, AssignedTask, Crisis, Land, Domain
 from .forms import RPEventCommentForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.http import Http404
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
 from server.utils.view_mixins import LimitPageMixin
+from PIL import Image, ImageDraw
 
 # Create your views here.
 
@@ -148,3 +149,101 @@ def event_comment(request, pk):
             form.post_comment(char, event)
             return HttpResponseRedirect(reverse('dominion:display_event', args=(pk,)))
     return HttpResponseRedirect(reverse('dominion:display_event', args=(pk,)))
+
+
+def map_image(request):
+    GRID_SIZE = 100
+
+    TERRAIN_COLORS = {
+        Land.COAST: '#a0a002',
+        Land.DESERT: '#ffff00',
+        Land.GRASSLAND: '#a0ff00',
+        Land.HILL: '#00ff00',
+        Land.MOUNTAIN: '#afafaf',
+        Land.OCEAN: '#000080',
+        Land.PLAINS: '#808000',
+        Land.SNOW: '#ffffff',
+        Land.TUNDRA: '#cccccc',
+        Land.FOREST: '#00aa00',
+        Land.JUNGLE: '#008800',
+        Land.MARSH: '#ff8000',
+        Land.ARCHIPELAGO: '#00ff00',
+        Land.FLOOD_PLAINS: '#00ff88',
+        Land.ICE: '#cfcfcf',
+        Land.LAKES: '#0000a0',
+        Land.OASIS: '#0000ff',
+    }
+
+    TERRAIN_NAMES = {
+        Land.COAST: 'Coastal',
+        Land.DESERT: 'Deset',
+        Land.GRASSLAND: 'Grassland',
+        Land.HILL: 'Hills',
+        Land.MOUNTAIN: 'Mountains',
+        Land.OCEAN: 'Ocean',
+        Land.PLAINS: 'Plains',
+        Land.SNOW: 'Snow',
+        Land.TUNDRA: 'Tundra',
+        Land.FOREST: 'Forest',
+        Land.JUNGLE: 'Jungle',
+        Land.MARSH: 'Marsh',
+        Land.ARCHIPELAGO: 'Archipelago',
+        Land.FLOOD_PLAINS: 'Flood Plains',
+        Land.ICE: 'Ice',
+        Land.LAKES: 'Lakes',
+        Land.OASIS: 'Oasis',
+    }
+
+    response = HttpResponse(content_type="image/png")
+
+    min_x = 0
+    min_y = 0
+    max_x = 0
+    max_y = 0
+
+    lands = Land.objects.all()
+    for land in lands:
+        min_x = min(min_x, land.x_coord)
+        min_y = min(min_y, land.y_coord)
+        max_x = max(max_x, land.x_coord)
+        max_y = max(max_y, land.y_coord)
+
+    total_width = max_x - min_x
+    total_height = max_y - min_y
+
+    mapimage = Image.new("RGB", (total_width * GRID_SIZE, total_height * GRID_SIZE), "#000080")
+    mapdraw = ImageDraw.Draw(mapimage)
+
+    try:
+        for land in lands:
+            x1 = (land.x_coord - min_x) * GRID_SIZE
+            y1 = (max_y - ((land.y_coord - min_y) - 1)) * GRID_SIZE
+            x2 = x1 + GRID_SIZE + 1
+            y2 = y1 + GRID_SIZE + 1
+            mapdraw.rectangle([(x1, y1), (x2, y2)], fill=TERRAIN_COLORS[land.terrain])
+
+            text_color = "black"
+            if (land.terrain == Land.LAKES) or (land.terrain == Land.OASIS):
+                text_color = "white"
+
+            text_x = x1 + 5
+            text_y = y1 + 10
+            mapdraw.text((text_x, text_y), "%s (%d,%d)\n%s" % (TERRAIN_NAMES[land.terrain], land.x_coord, land.y_coord,
+                                                               land.region.name), text_color)
+
+            domains = Domain.objects.filter(land=land)\
+                .filter(ruler__house__organization_owner__members__player__player__isnull=False)
+            text_x = x1 + 10
+            text_y = y1 + 60
+            if domains:
+                domain = domains[0]
+                mapdraw.text((text_x, text_y), domain.name, text_color)
+
+    except Exception as exc:
+        print str(exc)
+
+    # Delete our drawing tool and commit the image
+    del mapdraw
+
+    mapimage.save(response, "PNG")
+    return response
