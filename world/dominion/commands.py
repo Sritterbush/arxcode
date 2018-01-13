d4458da537baced431bb6671c3cd0122d2706d11
Tehom <surly.mime@gmail.com>
2018-01-13 18:07:49 -0500

    Add deletion of a few more npc assetowners, verbose display of what's deleted, exception for MilitaryUnits that originated with npcs

--- world/dominion/commands.py
+++ world/dominion/commands.py
@@ -40,7 +40,7 @@ class CmdAdmDomain(ArxPlayerCommand):
       @admin_domain/replacevassal receiver=domain_id, numvassals
       @admin_domain/createvassal receiver=liege_domain_id, numvassals
       @admin_domain/transferowner receiver=domain_id
-      @admdin_domain/transferrule char=domain_id
+      @admin_domain/transferrule char=domain_id
       @admin_domain/liege domain_id=family
       @admin_domain/list <fealty, eg: 'Velenosa'>
       @admin_domain/list_char player
@@ -3622,28 +3622,34 @@ class CmdCleanupDomain(ArxPlayerCommand):
     locks = "cmd:perm(Wizards)"
     help_category = "Dominion"
 
+    def delete_and_report(self, name, qs):
+        self.msg("Deleting NPC %s..." % name)
+        output = qs.delete()
+        output = "Total: %s, %s" % (output[0], ", ".join("%s: %s" % (key, value) for key, value in output[1].items()))
+        self.msg("Deleted: %s" % output)
+
     def func(self):
         domains = Domain.objects \
             .filter(ruler__house__organization_owner__members__player__player__isnull=True).distinct()
-        self.msg("Deleting NPC domains...")
-        domains.delete()
+        self.delete_and_report("domains", domains)
 
         assetowners = AssetOwner.objects.filter(organization_owner__isnull=False)\
             .filter(organization_owner__members__player__player__isnull=True).distinct()
-        self.msg("Deleting NPC asset owners...")
-        assetowners.delete()
+        self.delete_and_report("organization asset owners", assetowners)
+
+        npc_asset_owners = AssetOwner.objects.filter(organization_owner__isnull=True,
+                                                     player__player__isnull=True).distinct()
+        self.delete_and_report("PlayerOrNpc asset owners", npc_asset_owners)
 
         armies = Army.objects.filter(owner__isnull=True).distinct()
-        self.msg("Deleting orphaned NPC armies...")
-        armies.delete()
+        self.delete_and_report("armies", armies)
 
         rulers = Ruler.objects.filter(house__organization_owner__members__player__player__isnull=True).distinct()
-        self.msg("Deleting NPC rulers...")
-        rulers.delete()
+        self.delete_and_report("rulers", rulers)
 
         orgs = Organization.objects.filter(members__player__player__isnull=True).distinct()
-        self.msg("Deleting NPC organizations...")
-        orgs.delete()
+        MilitaryUnit.objects.filter(origin__in=orgs).update(origin=None)
+        self.delete_and_report("organizations", orgs)
 
         self.msg("Done!")
 
