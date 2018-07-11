dd8f096abda7db4e817d92d63ebfa384119f90a1
Tehom <surly.mime@gmail.com>
2018-07-11 02:27:19 -0400

    Add initial broker models, list deals method

--- world/petitions/models.py
+++ world/petitions/models.py
@@ -3,4 +3,36 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from evennia.utils.idmapper.models import SharedMemoryModel
+
+
+class BrokeredDeal(SharedMemoryModel):
+    """A deal sitting on the broker, waiting for someone to buy it"""
+    ACTION_POINTS = 0
+    ECONOMIC = 1
+    SOCIAL = 2
+    MILITARY = 3
+    CRAFTING_MATERIALS = 4
+    OFFERING_TYPES = ((ACTION_POINTS, "Action Points"), (ECONOMIC, "Economic Resources"), (SOCIAL, "Social Resources"),
+                      (MILITARY, "Military Resources"), (CRAFTING_MATERIALS, "Crafting Materials"))
+    owner = models.ForeignKey("dominion.PlayerOrNpc", related_name="brokered_sales")
+    offering_type = models.PositiveSmallIntegerField(default=ACTION_POINTS, choices=OFFERING_TYPES)
+    amount = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0)
+    buyers = models.ManyToManyField("dominion.PlayerOrNpc", related_name="brokered_purchases",
+                                    through="BrokeredPurchaseAmount")
+    crafting_material_type = models.ForeignKey("dominion.CraftingMaterialType", null=True, on_delete=models.CASCADE)
+
+    @property
+    def material_name(self):
+        """Returns the name of what we're offering"""
+        if self.crafting_material_type:
+            return self.crafting_material_type.name
+        return self.get_offering_type_display()
+
+
+class BrokeredPurchaseAmount(SharedMemoryModel):
+    """Details of a purchase by a player"""
+    deal = models.ForeignKey('BrokeredDeal', related_name="purchase_amounts")
+    buyer = models.ForeignKey('dominion.PlayerOrNpc', related_name="purchase_amounts")
+    amount = models.PositiveIntegerField(default=0)
--- world/petitions/petitions_commands.py
+++ world/petitions/petitions_commands.py
@@ -2,6 +2,7 @@
 Commands for petitions app
 """
 from server.utils.arx_utils import ArxCommand
+from world.petitions.models import BrokeredDeal
 
 
 class CmdPetition(ArxCommand):
@@ -25,3 +26,73 @@ class CmdPetition(ArxCommand):
     Create a petition that is either submitted to an organization or
     posted in the market for signups.
     """
+    key = "petition"
+    help_category = "Market"
+
+    def func(self):
+        """Executes petition command"""
+        pass
+
+
+class CmdBroker(ArxCommand):
+    """
+    Buy or sell AP/Resources in the market
+
+    Usage:
+        broker/search <type>
+        broker/buy <ID #>=<amount>
+        broker/sell <buyer>=<type>,<amount>
+        broker/cancel <ID #>
+
+    Allows you to automatically buy or sell crafting materials or
+    more abstract things such as influence with npcs (resources)
+    or time (action points). To sell or buy action points, specify
+    'action points' or 'ap' as the type. To sell or buy resources,
+    specify the type of resource (economic, social, or military).
+    """
+    key = "broker"
+    help_category = "Market"
+
+    def func(self):
+        """Executes broker command"""
+        if not self.args or "search" in self.switches:
+            return self.broker_display()
+        if not self.switches:
+            return self.display_deal_detail()
+        if "buy" in self.switches:
+            return self.make_purchase()
+        if "sell" in self.switches:
+            return self.make_sale_offer()
+
+    def broker_display(self):
+        """Displays items for sale on the broker"""
+        from server.utils.prettytable import PrettyTable
+        qs = BrokeredDeal.objects.filter(amount__gte=1)
+        if "search" in self.switches and self.args:
+            from django.db.models import Q
+            args = self.args.lower()
+            if args in ("ap", "action points", "action_points"):
+                query = Q(offering_type=BrokeredDeal.ACTION_POINTS)
+            elif "economic" in args:
+                query = Q(offering_type=BrokeredDeal.ECONOMIC)
+            elif "social" in args:
+                query = Q(offering_type=BrokeredDeal.SOCIAL)
+            elif "military" in args:
+                query = Q(offering_type=BrokeredDeal.MILITARY)
+            else:
+                query = Q(crafting_material_type__icontains=args) | Q(owner__player__username__iexact=args)
+            qs = qs.filter(query)
+
+        table = PrettyTable(["ID", "Seller", "Type", "Price", "Amount"])
+        for deal in qs:
+            table.add_row([deal.id, deal.owner, deal.material_name, deal.price, deal.amount])
+        self.msg(str(table))
+
+    def display_deal_detail(self):
+        pass
+
+    def make_purchase(self):
+        pass
+
+    def make_sale_offer(self):
+        pass
