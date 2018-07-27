034b37a48e7d59606e9e99a8a109a829387321b6
Dave Brannigan <tehom@arxmush.org>
2018-07-27 12:06:16 -0400

    Change kwargs to instead be formfields, will now be part of data

--- world/dominion/forms.py
+++ world/dominion/forms.py
@@ -3,8 +3,7 @@ Forms for Dominion
 """
 from django import forms
 
-from server.utils.exceptions import PayError
-from world.dominion.models import RPEvent
+from world.dominion.models import RPEvent, Organization, PlayerOrNpc
 
 
 class RPEventCommentForm(forms.Form):
@@ -13,6 +12,7 @@ class RPEventCommentForm(forms.Form):
     private = forms.BooleanField(initial=False, required=False)
 
     def post_comment(self, char, event):
+        """Posts a comment for an RPEvent"""
         msg = self.cleaned_data['journal_text']
         white = not self.cleaned_data['private']
         char.messages.add_event_journal(event, msg, white=white)
@@ -20,6 +20,11 @@ class RPEventCommentForm(forms.Form):
 
 class RPEventCreateForm(forms.ModelForm):
     """Form for creating a RPEvent. We'll actually try using it in commands for validation"""
+    hosts = forms.ModelMultipleChoiceField(queryset=PlayerOrNpc.objects.all())
+    invites = forms.ModelMultipleChoiceField(queryset=PlayerOrNpc.objects.all())
+    gms = forms.ModelMultipleChoiceField(queryset=PlayerOrNpc.objects.all())
+    org_invites = forms.ModelMultipleChoiceField(queryset=Organization.objects.all())
+
     class Meta:
         """Meta options for setting up the form"""
         model = RPEvent
@@ -28,10 +33,6 @@ class RPEventCreateForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         self.owner = kwargs.pop('owner')
-        self.hosts = kwargs.pop('hosts', [])
-        self.invites = kwargs.pop('invites', [])
-        self.org_invites = kwargs.pop('org_invites', [])
-        self.gms = kwargs.pop('gms', [])
         super(RPEventCreateForm, self).__init__(*args, **kwargs)
         self.fields['desc'].required = True
         self.fields['date'].required = True
@@ -41,32 +42,44 @@ class RPEventCreateForm(forms.ModelForm):
         """Returns the amount of money needed for validation"""
         return dict(RPEvent.LARGESSE_VALUES)[self.data.get('celebration_tier', 0)][0]
 
-    def pay_costs(self):
-        """Pays costs or adds an error"""
-        try:
-            self.owner.player.char_ob.pay_money(self.cost)
-        except PayError:
-            self.add_error('celebration_tier', "You cannot afford to pay the cost of %s." % self.cost)
-
     def clean(self):
         """Validates that we can pay for things. Any special validation should be here"""
         cleaned_data = super(RPEventCreateForm, self).clean()
-        self.pay_costs()
+        self.check_risk()
+        self.check_costs()
         return cleaned_data
+
+    def check_costs(self):
+        """Checks if we can pay, if not, adds an error"""
+        if self.cost > self.owner.player.char_ob.currency:
+            self.add_error('celebration_tier', "You cannot afford to pay the cost of %s." % self.cost)
+
+    def check_risk(self):
+        """Checks that our risk field is acceptable"""
+        gms = self.cleaned_data.get('gms', [])
+        risk = self.cleaned_data.get('risk', RPEvent.NORMAL_RISK)
+        if not any(gm for gm in gms if gm.player.is_staff or gm.player.check_permstring("builders")):
+            if risk != RPEvent.NORMAL_RISK:
+                self.add_error('risk', "Risk cannot be altered without a staff member as GM.")
 
     def save(self, commit=True):
         """Saves the instance and adds the form's owner as the owner of the petition"""
         event = super(RPEventCreateForm, self).save(commit)
-        for host in self.hosts:
+        for host in self.cleaned_data.get('hosts', []):
             event.add_host(host)
-        for gm in self.gms:
+        for gm in self.cleaned_data.get('gms', []):
             event.add_gm(gm)
-        for pc_invite in self.invites:
+        for pc_invite in self.cleaned_data.get('invites', []):
             event.add_guest(pc_invite)
-        for org in self.org_invites:
+        for org in self.cleaned_data.get('org_invites', []):
             event.invite_org(org)
+        self.pay_costs()
         self.post_event(event)
         return event
+
+    def pay_costs(self):
+        """Pays the costs of the event"""
+        self.owner.player.char_ob.pay_money(self.cost)
 
     def post_event(self, event):
         """Makes a post of this event"""
@@ -78,18 +91,25 @@ class RPEventCreateForm(forms.ModelForm):
     def display(self):
         """Returns a game-friend display string"""
         msg = "{wName:{n %s\n" % self.data.get('name')
+        msg += "{wMain Host:{n %s\n" % self.owner
+        hosts = PlayerOrNpc.objects.filter(id__in=self.data.get('hosts', []))
+        if hosts:
+            msg += "{wOther Hosts:{n %s\n" % ", ".join(str(ob) for ob in hosts)
         msg += "{wPublic:{n %s\n" % "Public" if self.data.get('public_event', True) else "Private"
         msg += "{wDescription:{n %s\n" % self.data.get('desc')
         msg += "{wDate:{n %s\n" % self.data.get('date')
         msg += "{wLocation:{n %s\n" % self.data.get('location')
         msg += "{wLargesse:{n %s\n" % dict(RPEvent.LARGESSE_CHOICES).get(self.data.get('celebration_tier', 0))
-        if self.gms:
-            msg += "{wGMs:{n %s\n" % ", ".join(str(ob) for ob in self.gms)
+        gms = PlayerOrNpc.objects.filter(id__in=self.data.get('gms', []))
+        if gms:
+            msg += "{wGMs:{n %s\n" % ", ".join(str(ob) for ob in gms)
             msg += "{wRisk:{n %s\n" % dict(RPEvent.RISK_CHOICES).get(self.data.get('risk', RPEvent.NORMAL_RISK))
-        if self.orgs:
+        orgs = PlayerOrNpc.objects.filter(id__in=self.data.get('orgs', []))
+        if orgs:
             msg += "{wOrg invitations:{n %s\n" % ", ".join(str(org) for org in self.orgs)
-        if self.invites:
-            msg += "{wInvitations:{n %s\n" % ", ".join(str(ob) for ob in self.invites)
+        invites = PlayerOrNpc.objects.filter(id__in=self.data.get('invites', []))
+        if invites:
+            msg += "{wInvitations:{n %s\n" % ", ".join(str(ob) for ob in invites)
         return msg
 
     def display_errors(self):
