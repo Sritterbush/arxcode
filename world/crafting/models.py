6180eb49a2f4229707c084313acd2d6b8f9b8465
Tehom <surly.mime@gmail.com>
2019-07-13 21:09:35 -0400

    Started adding more relationships. Pruned out some fields from CraftingRecipe, haven't yet converted code for it.

--- world/crafting/models.py
+++ world/crafting/models.py
@@ -4,10 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-from evennia.locks.lockhandler import LockHandler
 from evennia.utils import create
 from evennia.utils.idmapper.models import SharedMemoryModel
-from server.utils.arx_utils import CachedProperty
+from server.utils.arx_utils import CachedProperty, CachedPropertiesMixin
 
 
 class Mats(object):
@@ -43,12 +42,6 @@ class CraftingRecipe(CachedPropertiesMixin, SharedMemoryModel):
     desc = models.TextField(blank=True, null=True)
     # organizations or players that know this recipe
     known_by = models.ManyToManyField('dominion.AssetOwner', blank=True, related_name='recipes', db_index=True)
-    primary_materials = models.ManyToManyField('CraftingMaterialType', blank=True, related_name='recipes_primary')
-    secondary_materials = models.ManyToManyField('CraftingMaterialType', blank=True, related_name='recipes_secondary')
-    tertiary_materials = models.ManyToManyField('CraftingMaterialType', blank=True, related_name='recipes_tertiary')
-    primary_amount = models.PositiveSmallIntegerField(blank=0, default=0)
-    secondary_amount = models.PositiveSmallIntegerField(blank=0, default=0)
-    tertiary_amount = models.PositiveSmallIntegerField(blank=0, default=0)
     difficulty = models.PositiveSmallIntegerField(blank=0, default=0)
     additional_cost = models.PositiveIntegerField(blank=0, default=0)
     # the ability/profession that is used in creating this
@@ -58,37 +51,7 @@ class CraftingRecipe(CachedPropertiesMixin, SharedMemoryModel):
     type = models.CharField(blank=True, null=True, max_length=80)
     # level in ability this recipe corresponds to. 1 through 6, usually
     level = models.PositiveSmallIntegerField(blank=1, default=1)
-    # the result is a text field that we can later parse to determine what we create
-    result = models.TextField(blank=True, null=True)
     allow_adorn = models.BooleanField(default=True, blank=True)
-    # lockstring
-    lock_storage = models.TextField('locks', blank=True, help_text='defined in setup_utils')
-
-    def __init__(self, *args, **kwargs):
-        super(CraftingRecipe, self).__init__(*args, **kwargs)
-        self.locks = LockHandler(self)
-        self.resultsdict = self.parse_result(self.result)
-        self.materials = MatList()
-        # create throws errors on __init__ for many to many fields
-        if self.pk:
-            if self.primary_amount:
-                for mat in self.primary_materials.all():
-                    self.materials.mats.append(Mats(mat, self.primary_amount))
-            if self.secondary_amount:
-                for mat in self.secondary_materials.all():
-                    self.materials.mats.append(Mats(mat, self.secondary_amount))
-            if self.tertiary_amount:
-                for mat in self.tertiary_materials.all():
-                    self.materials.mats.append(Mats(mat, self.tertiary_amount))
-
-    def access(self, accessing_obj, access_type='learn', default=False):
-        """
-        Determines if another object has permission to access.
-        accessing_obj - object trying to access this one
-        access_type - type of access sought
-        default - what to return if no lock of access_type was found
-        """
-        return self.locks.check(accessing_obj, access_type=access_type, default=default)
 
     def org_owners(self):
         return self.known_by.select_related('organization_owner').filter(organization_owner__isnull=False)
@@ -97,26 +60,29 @@ class CraftingRecipe(CachedPropertiesMixin, SharedMemoryModel):
 
     def can_be_learned_by(self, learner):
         """Returns True if learner can learn this recipe, False otherwise"""
-        if not self.access(learner):
-            return False
+        # insert new check for skill/stat/ability here
+        pass
         # if we have no orgs that know this recipe, anyone can learn it normally
         if not self.org_owners:
             return True
         # check if they have granted access from any of the orgs that know it
         return any(ob.access(learner, access_type="recipe") for ob in self.org_owners)
 
-    @staticmethod
-    def parse_result(results):
-        """
-        Given a string, return a dictionary of the different
-        key:value pairs separated by semicolons
-        """
-        if not results:
-            return {}
-        rlist = results.split(";")
-        keyvalpairs = [pair.split(":") for pair in rlist]
-        keydict = {pair[0].strip(): pair[1].strip() for pair in keyvalpairs if len(pair) == 2}
-        return keydict
+    @CachedProperty
+    def cached_required_materials(self):
+        return list(self.required_materials.all())
+
+    @property
+    def primary_requirements(self):
+        return [ob for ob in self.cached_required_materials if ob.priority == 1]
+
+    @property
+    def secondary_requirements(self):
+        return [ob for ob in self.cached_required_materials if ob.priority == 2]
+
+    @property
+    def tertiary_requirements(self):
+        return [ob for ob in self.cached_required_materials if ob.priority == 3]
 
     def display_reqs(self, dompc=None, full=False):
         """Returns string display for recipe"""
@@ -125,45 +91,37 @@ class CraftingRecipe(CachedPropertiesMixin, SharedMemoryModel):
             msg += "{wName:{n %s\n" % self.name
             msg += "{wDescription:{n %s\n" % self.desc
         msg += "{wSilver:{n %s\n" % self.additional_cost
-        tups = ((self.primary_amount, "{wPrimary Materials:{n\n", self.primary_materials),
-                (self.secondary_amount, "\n{wSecondary Materials:{n\n", self.secondary_materials),
-                (self.tertiary_amount, "\n{wTertiary Materials:{n\n", self.tertiary_materials),)
-        for tup in tups:
-            if tup[0]:
-                msg += tup[1]
+        if dompc:
+            dompc_mats = dompc.assets.materials.all()
+        else:
+            dompc_mats = []
+        mat_msgs = []
+        for req in self.cached_required_materials:
+            if req.amount:
+                mat = req.type
                 if dompc:
-                    msglist = []
-                    for mat in tup[2].all():
-                        try:
-                            pcmat = dompc.assets.materials.get(type=mat)
-                            amt = pcmat.amount
-                        except CraftingMaterials.DoesNotExist:
-                            amt = 0
-                        msglist.append("%s: %s (%s/%s)" % (str(mat), tup[0], amt, tup[0]))
-                    msg += ", ".join(msglist)
+                    try:
+                        pcmat = [ob for ob in dompc_mats if ob.type == mat][0]
+                        amt = pcmat.amount
+                    except IndexError:
+                        amt = 0
+                    mat_msgs.append("%s: %s (%s/%s)" % (mat, req.amount, amt, mat))
                 else:
-                    msg += ", ".join("%s: %s" % (str(ob), tup[0]) for ob in tup[2].all())
+                    mat_msgs.append("%s: %s" % (mat, req.amount))
+        if mat_msgs:
+            msg += "Materials: %s" % ", ".join(mat_msgs)
         return msg
 
     @CachedProperty
     def value(self):
         """Returns total cost of all materials used"""
         val = self.additional_cost
-        for mat in self.primary_materials.all():
-            val += mat.value * self.primary_amount
-        for mat in self.secondary_materials.all():
-            val += mat.value * self.secondary_amount
-        for mat in self.tertiary_materials.all():
-            val += mat.value * self.tertiary_amount
+        for req in self.cached_required_materials:
+            val += req.material.value * req.amount
         return val
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name or "Unknown"
-
-    @property
-    def baseval(self):
-        """Returns baseval used in recipes"""
-        return float(self.resultsdict.get("baseval", 0.0))
 
 
 class CraftingMaterialType(SharedMemoryModel):
@@ -198,31 +156,51 @@ class CraftingMaterialType(SharedMemoryModel):
 
         result = create.create_object(key=name_string,
                                       typeclass="world.dominion.dominion_typeclasses.CraftingMaterialObject")
-        result.db.desc = self.desc
-        result.db.material_type = self.id
-        result.db.quantity = quantity
+        self.object_materials.create(object=result, amount=quantity)
         return result
 
 
-class CraftingMaterials(SharedMemoryModel):
-    """
-    Materials used for crafting. Can be stored by an AssetOwner as part of their
-    collection, -or- used in a recipe to measure how much they need of a material.
-    If it is used in a recipe, do NOT set it owned by any asset owner, or by changing
-    the amount they'll change the amount required in a recipe!
-    """
-    type = models.ForeignKey('CraftingMaterialType', blank=True, null=True, db_index=True)
-    amount = models.PositiveIntegerField(blank=0, default=0)
-    owner = models.ForeignKey('dominion.AssetOwner', blank=True, null=True, related_name='materials', db_index=True)
+class AbstractMaterialAmount(models.Model):
+    type = models.ForeignKey("crafting.CraftingMaterialType", on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField(default=0)
 
     class Meta:
-        """Define Django meta options"""
-        verbose_name_plural = "Crafting Materials"
+        abstract = True
 
-    def __unicode__(self):
+    def __str__(self):
         return "%s %s" % (self.amount, self.type)
 
     @property
     def value(self):
         """Returns value of materials they have"""
         return self.type.value * self.amount
+
+
+class RecipeRequirement(AbstractMaterialAmount):
+    recipe = models.ForeignKey("crafting.CraftingRecipe", on_delete=models.CASCADE)
+    priority = models.PositiveSmallIntegerField(default=1)
+
+    class Meta:
+        default_related_name = "required_materials"
+        verbose_name_plural = "Recipe Materials Requirements"
+
+
+class Adornment(AbstractMaterialAmount):
+    """Materials contained within an object, such as adornments, or when used as a loot object"""
+    object = models.ForeignKey('objects.ObjectDB', on_delete=models.CASCADE)
+
+    class Meta:
+        default_related_name = "object_materials"
+        verbose_name_plural = "Adornments"
+
+
+class OwnedMaterial(AbstractMaterialAmount):
+    """
+    Crafting Materials owned by some AssetOwner
+    """
+    owner = models.ForeignKey('dominion.AssetOwner', on_delete=models.CASCADE)
+
+    class Meta:
+        """Define Django meta options"""
+        verbose_name_plural = "Owned Materials"
+        default_related_name = "materials"
